c5661b3577a15c81aa3f731c57990f0a31a40af5
pratamafarhan10 <pratamafarhan10@gmail.com>
2021-10-05 11:43:13 +0700

    landing page done

---
+++ src/components/molecules/Navbar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="../../assets/logo.png" alt="Logo" width="250" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/equality-demand"
+              >Equality Demand</router-link
+            >
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/jobs-for-women"
+              >Jobs for Women</router-link
+            >
+          </li>
+        </ul>
+        <a href="#" class="btn btn-outline-success my-2 my-sm-0">Login</a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+a.active {
+  border-bottom: solid;
+  border-bottom-color: #bd02ff;
+  font-weight: bold;
+}
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,127 @@
+<template>
+  <!-- Hero -->
+  <section class="pt-4 pt-md-5 mt-5">
+    <div class="row align-items-center">
+      <div class="col-12 col-md-5 col-lg-6">
+        <p class="text-lg text-start text-md-start">Welcome to</p>
+        <p class="text-lg font-weight-bold">
+          <span style="color: #bd02ff">#</span>WomenDeserveBetter
+        </p>
+        <p class="text-secondary">
+          One of the causes of the gender wage gap is discrimination. Society
+          must confront cultural biases that continue to harm women by devaluing
+          their work and confining them to specific gender roles.
+        </p>
+        <a class="btn btn-primary btn-purple" href="/equality-demand"
+          >Demand Equality Now</a
+        >
+      </div>
+      <div class="col-12 col-md-7 col-lg-6">
+        <img
+          src="../assets/womenIllustration.svg"
+          alt="women illustration"
+          class="img-fluid"
+        />
+      </div>
+    </div>
+  </section>
+
+  <!-- Why women deserve better? -->
+  <section class="mt-5">
+    <div class="row justify-content-center mb-3">
+      <h1>Why Women Deserve Better?</h1>
+    </div>
+    <div class="row align-items-center">
+      <div class="col-12 col-md-6">
+        <img
+          src="../assets/men1.png"
+          alt="women illustration"
+          class="img-fluid"
+        />
+      </div>
+      <div class="col-12 col-md-6">
+        <p class="text-md">
+          Compared to men, women are less likely to work full-time, more likely
+          to be employed in lower-paid occupations, and less likely to progress
+          in their careers. As a result gender pay gaps persist and women are
+          more likely to end their lives in poverty.
+        </p>
+      </div>
+    </div>
+  </section>
+
+  <!-- What causes the gender wage gap? -->
+  <section class="mt-5">
+    <div class="row justify-content-center">
+      <h1>What Causes The Gender Wage Gap?</h1>
+    </div>
+    <div class="row align-items-start">
+      <div class="col-12 col-md-6 d-flex justify-content-center mt-3">
+        <div class="card more-rounded">
+          <div class="card-body">
+            <h5 class="card-title text-center">Differences in industries or jobs worked</h5>
+            <p class="card-text text-start">
+              So-called women’s jobs, which are jobs that have historically had majority-female workforces, such as home health aides and child care workers, tend to offer lower pay and fewer benefits than so-called men’s jobs, which are jobs that have had predominantly male workforces, including jobs in trades such as building and construction.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 d-flex justify-content-center mt-3">
+        <div class="card more-rounded">
+          <div class="card-body">
+            <h5 class="card-title text-center">Differences in years of experience</h5>
+            <p class="card-text text-start">
+              Women are disproportionately driven out of the workforce to accommodate caregiving and other unpaid obligations and thus tend to have less work experience than men.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 d-flex justify-content-center mt-3">
+        <div class="card more-rounded">
+          <div class="card-body">
+            <h5 class="card-title text-center">Differences in hours worked</h5>
+            <p class="card-text text-start">
+              Because women tend to work fewer hours to accommodate caregiving and other unpaid obligations, they are also more likely to work part time, which means lower hourly wages and fewer benefits compared with full-time workers.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-6 d-flex justify-content-center mt-3">
+        <div class="card more-rounded">
+          <div class="card-body">
+            <h5 class="card-title text-center">Discrimination</h5>
+            <p class="card-text text-start">
+              Gender-based pay discrimination has been illegal since 1963 but is still a frequent, widespread practice—particularly for women of color. It can thrive especially in workplaces that discourage open discussion of wages and where employees fear retaliation.
+            </p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.text-lg {
+  font-size: 2rem;
+}
+
+.text-md {
+  font-size: 1.2rem;
+}
+
+.btn-purple {
+  border: none;
+  background-color: #a91adc;
+}
+
+.btn-purple:hover {
+  background-color: #b000ee;
+}
+
+.more-rounded{
+    border-radius: 10px;
+    border: none;
+    box-shadow: 2px 2px 1px #b000ee;
+}
+</style>
